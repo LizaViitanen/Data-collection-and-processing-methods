565316583993340ceee96bea359b02b9c3fce1e2
Liza <LizaViitanen@gmail.com>
2020-12-28 21:40:17 +0300

    1 задание 5 урока

---
+++ 5_2.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+from pprint import pprint
+from selenium.webdriver.common.action_chains import ActionChains
+from pymongo import MongoClient
+
+chrome_options = Options()
+chrome_options.add_argument('start-maximized')
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get('https://www.mvideo.ru')
+
+hits = driver.find_element_by_xpath('//div[contains(text(), "Хиты продаж")]/ancestor::div[@class="section"]')
+all_goods = []
+while True:
+    goods = hits.find_elements_by_class_name('fl-product-tile c-product-tile fl-product-tile_gallery-product-set')
+    for good in goods:
+        name = good.find_elements_by_css_selector('//h4').title
+
